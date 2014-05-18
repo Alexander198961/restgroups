5b2ff25122dcf4c8892d6b6043fe672cd459f5e1
Alexander198961 <alexander198961@gmail.com>
2014-05-18 11:20:54 +0300

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-from subprocess import Popen, PIPE
-import web
-import os
-import re
-from cgroup import *  
-from types import MethodType
-p1 = Popen(["./run.sh"], stdout=PIPE).communicate()
-print p1
